cc01d8e4301b69ceff94767ef85595124773671b
정다운 <wjdek88@naver.com>
2021-05-04 00:38:06 +0900

    Remind of 4rd Question of 2020 Kakao Internship

--- BFS_DFS/pro_2020_intern_4.py
+++ BFS_DFS/pro_2020_intern_4.py
@@ -1,61 +1,52 @@
 # 경주로 건설 (https://programmers.co.kr/learn/courses/30/lessons/67259)
-
+# https://hose0728.tistory.com/76
 # BFS - > 주로 두 노드 (0,0) -> (n-1,n-1) 사이의 최단 경로를 찾고 싶을 때
 # 탐색하면서 -> 직선이라면 100원, 코너라면 500원 추가
 from collections import deque
-
 board = [[0,0,0],[0,0,0],[0,0,0]]
-
-def dfs(x,y,board,n,price):
-    if x <= -1 or y <= -1 or x >= n or y >= n:
-        return False
-    if board[x][y] == 0:
-        board[x][y] = 2
-
-        # 직선
-        if board[x-1][y] == 2:
-            # 코너
-            if board[x-1][y-1] == 2 or board[x-1][y+1] == 2:
-                price += 500
-            else:
-                price += 100
-
-        elif board[x][y-1] == 2:
-            if board[x-1][y-1] == 2 or board[x+1][y-1] == 2:
-                price += 500
-            else:
-                price += 100
-        
-        elif board[x+1][y] == 2:
-            if board[x+1][y-1] == 2 or board[x+1][y+1] == 2:
-                price += 500
-            else:
-                price += 100
-        
-        elif board[x][y+1] == 2:
-            if board[x-1][y+1] == 2 or board[x+1][y+1] == 2:
-                price += 500
-            else:
-                price += 100
-        print(x,y,price)
-        dfs(x-1,y,board,n,price)
-        dfs(x+1,y,board,n,price)
-        dfs(x,y-1,board,n,price)
-        dfs(x,y+1,board,n,price)
-
-        return price
-    return price
 
 
 def solution(board):
+    n = len(board)
 
-    answer = 0
-    leng = len(board)
-    # dfs go
-    for x in range(leng):
-        for y in range(leng):
-            answer = dfs(x,y,board,leng,100)
+    dx = [-1,0,1,0]
+    dy = [0,-1,0,1]
+    answer = int(1e9)
+    queue =  deque()
+    # x좌표 & y좌표 방향 비용 순서로 push, (방향은 맨처음 시 -1로 설정)
+    # 0 ~ 3 => direction index!
+    queue.append((0,0,-1,0))
+    # Set Dict {(x,y,direction) : cost }
+    visited = {(0,0,0) : 0, (0,0,1): 0, (0,0,2): 0, (0,0,3): 0}
 
+    # BFS (For min-cost == min route)
+    while queue:
+        x, y, dir1, cost = queue.popleft() 
+        
+        for d in range(4):
+            nx = x + dx[d]
+            ny = y + dy[d]
+            # 범위 안 에서 + 방문하지 않은 노드에 대해
+            if -1 < nx < n and -1 < ny < n and not board[nx][ny]:
+                newcost = cost
+                # direction == -1, 방금 출발한 경우 => 방향이 없으므로 그냥 100 추가
+                if dir1 == -1:
+                    newcost += 100
+                # 기존 방향과 진행 방향이 평행한 경우
+                elif (dir1 - d) % 2 == 0:
+                    newcost += 100
+                # 기존 방향과 수직인 경우
+                else:
+                    newcost += 600
+
+                # 목적지 도착 경우, 정답 값과 비교 => min한 값으로 갱신
+                if nx == n - 1 and ny == n - 1 :
+                    answer = min(answer, newcost)
+                
+                # 기존에 방문하지 않았거나, 방문했을 경우보다 비용이 적은 경우 => queue.append
+                elif visited.get((nx,ny,d)) is None or visited.get((nx,ny,d)) > newcost:
+                    visited[(nx,ny,d)] = newcost
+                    queue.append((nx,ny,d,newcost))
     return answer
 
 print(solution(board))
